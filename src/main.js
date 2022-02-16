4bf34e18d78a5a04b2c32ccd4a90a163319cbe4d
Garavirod <rodrigogarciaavila26@gmail.com>
2022-02-15 20:19:24 -0600

    sass was add into project

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+/* Importación de estilos SCSS */
+import './styles/styles.scss'
 
 createApp(App).use(router).mount('#app')
