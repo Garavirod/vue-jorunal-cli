951969ad9cbfa57390b269d8ea3a4d5930013538
Garavirod <rodrigogarciaavila26@gmail.com>
2022-02-17 18:58:27 -0600

    Router daybook was separated in its own router modules, it was based on lazyload

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    <h1>Daybook</h1>
+    <button class="btn btn-primary" @click="goToDayBook()">Daybook</button>
   </div>
 </template>
+<script>
+export default {
+  methods:{
+     goToDayBook(){
+      this.$router.push({name:'daybook'});
+    }
+  }
+}
+</script>
