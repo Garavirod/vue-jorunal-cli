41a9db8fbe2e5d0044ee29cfbfb993d5003ab758
Garavirod <rodrigogarciaavila26@gmail.com>
2022-02-17 20:12:50 -0600

    styles class were added into EntryComponent

--- src/modules/daybook/components/EntryList.vue
+++ src/modules/daybook/components/EntryList.vue
@@ -1,6 +1,15 @@
 <template>
-    <div>
-        <h1>Entry List</h1>
+    <div class="entry-list-container">
+        <div class="px-2 pt-2">
+            <input type="text"
+            class="form-control"
+            placeholder="Shearch entries"
+            >
+        </div>
+
+        <div class="entry-scrollarea">
+            <h2 v-for="item in [1,2,3,4,5,6,7,9,10]" :key="item">Entry</h2>
+        </div>
     </div>
 </template>
 
@@ -14,3 +23,12 @@ export default {
     },
 }
 </script>
+
+<style lang="scss" scoped>
+    .entry-list-container{
+
+    }
+    .entry-scrollarea{
+        
+    }
+</style>
