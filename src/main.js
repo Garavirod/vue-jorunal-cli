a7c90733e48954968a565a022236ec4a53273bbf
Garavirod <rodrigogarciaavila26@gmail.com>
2022-02-28 17:55:02 -0600

    were defined actions, mutation and getter on journal'store

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
 /* Importación de estilos SCSS */
 import './styles/styles.scss'
 
-createApp(App).use(router).mount('#app')
+createApp(App)
+    .use( store )
+    .use(router)
+    .mount('#app')
